deaa7c990872b594a7bb00bb37883aa33b804593
Zero Ex <0xb4dc0d3x@gmail.com>
2023-02-08 19:20:07 +0330

    rot13

---
+++ src/main/java/org/code/methods/Rot13.java
@@ -0,0 +1,18 @@
+package org.code.methods;
+
+public class Rot13 {
+    Caeser caeser = new Caeser();
+    public String rot13(String text, boolean isTextEncrypted)
+    {
+        String newText = "";
+        if (!isTextEncrypted)
+        {
+            newText = caeser.caesar(text, 13, false);
+        }
+        if (isTextEncrypted)
+        {
+            newText = caeser.caesar(text, 13, true);
+        }
+        return newText;
+    }
+}
