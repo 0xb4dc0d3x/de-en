4379ad8edec62ac3658f610a29f3bc53cc930a4d
Zero Ex <0xb4dc0d3x@gmail.com>
2023-02-08 19:18:28 +0330

    atbash

---
+++ src/main/java/org/code/methods/AtBash.java
@@ -0,0 +1,27 @@
+package org.code.methods;
+
+public class AtBash {
+    public String atbash(String text)
+    {
+        char textBefore[] = text.toCharArray();
+        int length = text.length();
+        char textAfter[] = new char[length];
+        for (int i = 0; i < length; i++)
+        {
+            char letter = textBefore[i];
+            int numBefore = (int) letter;
+            if ((numBefore <= 96) && (numBefore >= 0))
+            {
+                textAfter[i] = letter;
+                continue;
+            }
+            int numAfter = 32;
+            numAfter = 122 - numBefore + 97;
+            char newLetter = (char) numAfter;
+
+            textAfter[i] = newLetter;
+        }
+        String newText = new String(textAfter);
+        return newText;
+    }
+}
