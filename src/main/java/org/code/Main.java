ce2429c5844d3506ce6a72248a12b843e5cac4c5
Zero Ex <0xb4dc0d3x@gmail.com>
2023-02-08 19:19:38 +0330

    init

---
+++ src/main/java/org/code/Main.java
@@ -0,0 +1,11 @@
+package org.code;
+
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        View v = new View();
+
+    }
+}
