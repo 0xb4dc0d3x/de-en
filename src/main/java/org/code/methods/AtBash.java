d69795489fa66ed0ac3c7f7e0004bed04767901f
Zero Ex <0xb4dc0d3x@gmail.com>
2023-02-09 00:58:56 +0330

    optimize variable

--- src/main/java/org/code/methods/AtBash.java
+++ src/main/java/org/code/methods/AtBash.java
@@ -2,23 +2,21 @@ package org.code.methods;
 
 public class AtBash {
     public String atbash(String text) {
-        char textBefore[] = text.toCharArray();
+        char[] textBefore = text.toCharArray();
         int length = text.length();
-        char textAfter[] = new char[length];
+        char[] textAfter = new char[length];
         for (int i = 0; i < length; i++) {
             char letter = textBefore[i];
-            int numBefore = (int) letter;
-            if ((numBefore <= 96) && (numBefore >= 0)) {
+            if (((int) letter <= 96) && ((int) letter >= 0)) {
                 textAfter[i] = letter;
                 continue;
             }
             int numAfter = 32;
-            numAfter = 122 - numBefore + 97;
+            numAfter = 122 - (int) letter + 97;
             char newLetter = (char) numAfter;
 
             textAfter[i] = newLetter;
         }
-        String newText = new String(textAfter);
-        return newText;
+        return new String(textAfter);
     }
 }
